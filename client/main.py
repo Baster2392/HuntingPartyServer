b8f3582574f8f3cb06bb70968af97f3e05e614d7
Baster2392 <kuba.slawinski.2003@gmail.com>
2024-03-20 19:32:23 +0100

    Init commit

---
+++ client/main.py
@@ -0,0 +1,37 @@
+import socket
+
+
+def main():
+    server_address = '127.0.0.1'
+    server_port = 5000
+
+    # Create socket of client
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Try to connect to server
+        client_socket.connect((server_address, server_port))
+
+        # Read messages from server
+        print("Otrzymano wiadomości od serwera:")
+
+        # Send message to server
+        while True:
+            message = input("Wpisz wiadomość (pusty wiersz aby zakończyć): ")
+            if not message:
+                break
+            client_socket.sendall(message.encode())
+
+            # Get answer from server
+            server_response = client_socket.recv(1024).decode()
+            print("Odpowiedź serwera:", server_response)
+
+    except ConnectionRefusedError:
+        print("Nie można połączyć się z serwerem.")
+    finally:
+        # Close socket
+        client_socket.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ server/socket-client.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 5000
+
+int main() {
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char* hello = "Hello from client";
+    char buffer[1024] = { 0 };
+
+    // Tworzenie gniazda klienta
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket creation failed");
+        return -1;
+    }
+
+    // Konfiguracja struktury adresu serwera
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Konwersja adresu IP z kropkowymi dziesiêtnymi na binarne
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        return -1;
+    }
+
+    // Nawi¹zanie po³¹czenia z serwerem
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
+        return -1;
+    }
+
+    // Wys³anie wiadomoœci do serwera
+    send(sock, hello, strlen(hello), 0);
+    printf("Hello message sent to server\n");
+
+    // Odbiór odpowiedzi od serwera
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+
+    return 0;
+}
